5bf2ef8049feb9f3a4e1c2266cc205d6ef3ffb80
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2019-07-19 15:21:34 +0545

    process.env.PORT

--- main.js
+++ main.js
@@ -21,6 +21,10 @@ app.get("/:id", (req, res, next) => {
     );
 });
 
-app.listen(8085, () => {
-  console.log("Server listening @ 8085");
+//use alternate localhost and the port Heroku assigns to $PORT
+const host = "0.0.0.0";
+const port = process.env.PORT || 8085;
+
+app.listen(port, host, () => {
+  console.log("Server listening @" + port);
 });
